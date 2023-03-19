b00f017e37f8f7a2be18633070cd9bc3ae5d8cb9
irumaa7 <91621451+Qupiee@users.noreply.github.com>
2023-03-19 16:25:45 +0700

    Update vehicle-purchase.js

--- vehicle-purchase.js
+++ vehicle-purchase.js
@@ -32,7 +32,9 @@ chooseVehicle('Volkswagen Beetle', 'Volkswagen Golf');
 3. Calculate an estimation for the price of a used vehicle
 
 Now that you made your decision you want to make sure you get a fair price at the dealership.
-Since you are interested in buying a used vehicle, the price depends on how old the vehicle is. For a rough estimate, assume if the vehicle is less than 3 years old, it costs 80% of the original price it had when it was brand new. If it is more than 10 years old, it costs 50%. If the vehicle is at least 3 years old but not older than 10 years, it costs 70% of the original price.
+Since you are interested in buying a used vehicle, the price depends on how old the vehicle is.
+For a rough estimate, assume if the vehicle is less than 3 years old, it costs 80% of the original price it had when it was brand new.
+If it is more than 10 years old, it costs 50%. If the vehicle is at least 3 years old but not older than 10 years, it costs 70% of the original price.
 
 Implement the calculateResellPrice(originalPrice, age) function that applies this logic using if,
 else if and else (there are other ways if you want to practice). It takes the original price and the age
